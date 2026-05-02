30bfb3789a7255ab3408ed39266ed41005fa0508
ShadoV90 <major0406@gmail.com>
2026-05-02 11:03:21 +0200

    modified:   tuning/xml_edit.sh 	modified:   tuningDIY.txt 	modified:   webroot/index.html 	modified:   webroot/js/config/configmodel.js Adjust ShadoV Favorite 3 virtualizer (it's awesome!)

--- tuning/xml_edit.sh
+++ tuning/xml_edit.sh
@@ -360,19 +360,24 @@ apply_virtual_bass() {
 				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-compressor-tuning value=\"[^\"]*\"|virtual-bass-compressor-tuning value=\"1,$((hbasscompstrength*16)),$((hbasscompstrength*16)),$((hbasscompstrength*24)),32,25,50\"|g"
 			fi
 			
+			if [ "$hbasslingain" -lt "$hbassharmboost" ]; then
+				harmlindiff=$(((hbassharmboost-hbasslingain)*20))
+			else
+				harmlindiff=0
+			fi
 			# Calculating formula for virtual bass
 			# Multiplying by 0 is intentional for testing purposes
 			if [ "$hbassharmtype" -eq 1 ]; then
-				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$((hbasslingain*0)),$((hbassharmboost*25)),$((hbassharmboost*10)),$((hbassharmboost*10))\"/>|g"
+				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$harmlindiff,$((hbassharmboost*25)),$((hbassharmboost*10)),$((hbassharmboost*10))\"/>|g"
 				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-hybgains .*|virtual-bass-hybgains value=\"$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0))\"/>|g"
 			elif [ "$hbassharmtype" -eq 2 ]; then
-				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$((hbasslingain*0)),$((hbassharmboost*25)),$((hbassharmboost*60)),$((hbassharmboost*60))\"/>|g"
+				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$harmlindiff,$((hbassharmboost*25)),$((hbassharmboost*40)),$((hbassharmboost*40))\"/>|g"
 				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-hybgains .*|virtual-bass-hybgains value=\"$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0))\"/>|g"
 			elif [ "$hbassharmtype" -eq 3 ]; then
-				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$((hbasslingain*0)),$((hbassharmboost*10)),$((hbassharmboost*80)),$((hbassharmboost*80))\"/>|g"
+				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$harmlindiff,$((hbassharmboost*25)),$((hbassharmboost*80)),$((hbassharmboost*80))\"/>|g"
 				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-hybgains .*|virtual-bass-hybgains value=\"$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0))\"/>|g"
 			elif [ "$hbassharmtype" -eq 4 ]; then
-				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$((hbasslingain*0)),$((hbassharmboost*30)),$((hbassharmboost*30)),$((hbassharmboost*30))\"/>|g"
+				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-harmgains .*|virtual-bass-harmgains value=\"$harmlindiff,$((hbassharmboost*30)),$((hbassharmboost*30)),$((hbassharmboost*30))\"/>|g"
 				echo "/endpoint_type=\"$endpoint\"/,/<\/tuning>/s|virtual-bass-hybgains .*|virtual-bass-hybgains value=\"$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0)),$((hbasslingain*0))\"/>|g"
 			fi
 		else
